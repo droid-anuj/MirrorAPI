a31aaaacea74474ef22aaea335deb4e4abb01ec6
Anujydv123 <anuj.yadav@bigfig.ai>
2026-05-06 18:05:18 +0530

    add specific production domain to trusted origins

--- MirrorApiBackend/MirrorApiBackend/settings.py
+++ MirrorApiBackend/MirrorApiBackend/settings.py
@@ -171,6 +171,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://mirrorapi-bjil.onrender.com",
     "https://*.onrender.com",
     "https://*.vercel.app",
 ]
