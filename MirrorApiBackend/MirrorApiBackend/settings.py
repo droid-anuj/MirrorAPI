e051923140a5c694ed85e9f1029b199378a4cd7b
Anujydv123 <anuj.yadav@bigfig.ai>
2026-05-06 18:04:50 +0530

    fix csrf 403 error

--- MirrorApiBackend/MirrorApiBackend/settings.py
+++ MirrorApiBackend/MirrorApiBackend/settings.py
@@ -166,4 +166,11 @@ else:
 
 # For development — allow all origins
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://*.onrender.com",
+    "https://*.vercel.app",
+]
